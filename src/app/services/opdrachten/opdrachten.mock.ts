b9e5dd540c1a4568b339e4deab55cfa8ec596e50
Maarten Paauw <maartenpaauw@gmail.com>
2016-10-09 11:38:12 +0200

    Opdachten worden nu vanaf Google Drive ingeladen

--- src/app/services/opdrachten/opdrachten.mock.ts
+++ src/app/services/opdrachten/opdrachten.mock.ts
@@ -8,11 +8,11 @@ export const OPDRACHTEN: Opdracht[] = [
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit! Aenean commodo ligula eget dolor! Aenean massa! Cum sociis natoque penatibus et magnis dis parturientmontes, nascetur ridiculus mus? Donec quam felis, ultricies nec, pellentesqueeu, pretium quis, sem; Nulla consequat massa quis enim! Donec pede justo,fringilla vel, aliquet nec, vulputate eget, arcu! In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo."
     ],
     images: [
-      "assets/opdrachten/opdracht_1/opdracht.png"
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nb2NSQnN2eEFTbzQ"
     ],
-    essay: "",
+    essay: "https://drive.google.com/open?id=1QBm8TnwuSr83Ryzl5pd_9kjDYwsYBoS40U7cJZXLCZs",
     peer_review: "",
-    opdracht: "assets/opdrachten/opdracht_1/opdracht.png",
+    opdracht: "https://drive.google.com/open?id=0BwxJxS6uD-_nb2NSQnN2eEFTbzQ",
     is_even: false
   },
   {
@@ -23,11 +23,11 @@ export const OPDRACHTEN: Opdracht[] = [
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit! Aenean commodo ligula eget dolor! Aenean massa! Cum sociis natoque penatibus et magnis dis parturientmontes, nascetur ridiculus mus? Donec quam felis, ultricies nec, pellentesqueeu, pretium quis, sem; Nulla consequat massa quis enim! Donec pede justo,fringilla vel, aliquet nec, vulputate eget, arcu! In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.",
     ],
     images: [
-      "assets/opdrachten/opdracht_2/opdracht.png"
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nSmcyVkVwNF8yWUk"
     ],
-    essay: "",
+    essay: "https://drive.google.com/open?id=1xgNPFaT8togyfLeAK58T5ZkG1rXVMCIapn_gVsdxQX8",
     peer_review: "",
-    opdracht: "assets/opdrachten/opdracht_2/opdracht.png",
+    opdracht: "https://drive.google.com/open?id=0BwxJxS6uD-_nem9UOGVOZXI4dWs",
     is_even: true
   },
   {
@@ -37,11 +37,11 @@ export const OPDRACHTEN: Opdracht[] = [
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit! Aenean commodo ligula eget dolor! Aenean massa! Cum sociis natoque penatibus et magnis dis parturientmontes, nascetur ridiculus mus? Donec quam felis, ultricies nec, pellentesqueeu, pretium quis, sem; Nulla consequat massa quis enim! Donec pede justo,fringilla vel, aliquet nec, vulputate eget, arcu! In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo."
     ],
     images: [
-      "assets/opdrachten/opdracht_3/opdracht.png"
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nZ05VTmhINTFmeVk"
     ],
-    essay: "",
+    essay: "https://drive.google.com/open?id=13Z0EwjIlaUnLgStKx89Df-1jrZzyQOxBRgXmulPtQvA",
     peer_review: "",
-    opdracht: "assets/opdrachten/opdracht_3/opdracht.png",
+    opdracht: "https://drive.google.com/open?id=0BwxJxS6uD-_nT2FkLUdhY09Malk",
     is_even: false
   },
   {
@@ -51,16 +51,16 @@ export const OPDRACHTEN: Opdracht[] = [
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit! Aenean commodo ligula eget dolor! Aenean massa! Cum sociis natoque penatibus et magnis dis parturientmontes, nascetur ridiculus mus? Donec quam felis, ultricies nec, pellentesqueeu, pretium quis, sem; Nulla consequat massa quis enim! Donec pede justo,fringilla vel, aliquet nec, vulputate eget, arcu! In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo."
     ],
     images: [
-      "assets/opdrachten/opdracht_4/slide_1.png",
-      "assets/opdrachten/opdracht_4/slide_2.png",
-      "assets/opdrachten/opdracht_4/slide_3.png",
-      "assets/opdrachten/opdracht_4/slide_4.png",
-      "assets/opdrachten/opdracht_4/slide_5.png",
-      "assets/opdrachten/opdracht_4/slide_6.png"
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nQXd3dUtPYmNUMkk",
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nazluUjJMb044VGs",
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nT3pzZFJxU1p1QUE",
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nSG1fMF9Ob0taTFU",
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nYUxiWnk1UzdjUkk",
+      "https://drive.google.com/uc?export=view&id=0BwxJxS6uD-_nOHpNZjNLakpYV1E"
     ],
-    essay: "",
+    essay: "https://drive.google.com/open?id=1RhZ7Yiy-Xer6jBuMkz9x9hzvm8niZKfTgTIZxhUZyOg",
     peer_review: "",
-    opdracht: "assets/opdrachten/opdracht_4/slide_1.png",
+    opdracht: "https://docs.google.com/presentation/d/1S2hyiL-Y36q17cRAjjQgDzylStYYGOnkmdAvSFc4Y7c/pub?start=false&loop=false&delayms=20000&slide=id.g1186acfbaf_0_0",
     is_even: true
   }
 ];
