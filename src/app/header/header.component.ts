69b64213bfbfe083cc01d4069f4a48d90e4d4c30
Owain van Brakel <owain.vanbrakel@gmail.com>
2016-10-10 02:00:39 +0200

    Missed this one

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,14 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
   @Input() private text: Array<string>;
 
   constructor() {
-  }
-
-  ngOnInit(): void {
-
   }
 }
