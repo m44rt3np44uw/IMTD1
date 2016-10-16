a06391c7067292b423649448e9e48a1beff42294
Maarten Paauw <maartenpaauw@gmail.com>
2016-10-16 16:52:19 +0200

    Over mij tekst toegevoegd

--- src/app/ik/ik.component.ts
+++ src/app/ik/ik.component.ts
@@ -7,6 +7,14 @@ import {Component} from "@angular/core";
 })
 export class IkComponent {
 
+  private ik: string;
+
   constructor() {
+    this.ik = "Mijn naam is Maarten Paauw, ben 23 jaar oud en woon in Den Haag samen met " +
+      "mijn vriendin. Ik ben tweedejaars student van de opleiding Informatica aan de Hogeschool " +
+      "Leiden. Voordat ik aan deze opleiding begon heb ik twee MBO opleidingen gedaan op het " +
+      "gebied van grafische vormgeving en techniek. Naast mijn huidige studie werk ik als " +
+      "freelancer voor verschillende opdrachtgevers. Hierbij bouw ik websites, ontwerp advertenties " +
+      "en ander drukwerk en doe beheer- en advieswerkzaamheden.";
   }
 }
